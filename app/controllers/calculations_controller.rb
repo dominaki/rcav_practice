9822ad91289358cc53d188b730ddb0206d219810
dominaki <dominaki@gmail.com>
2015-07-23 17:57:58 -0500

    first_attempt

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,10 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+
+def square_root_8
+render("square_root_8.html.erb")
 end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/8", { :controller => "calculations", :action => "square_root_8" })
+
+
 end
